2fa592887650ca846560630afa72f6e068c88512
MErenAydin <merenep12@gmail.com>
2020-12-10 14:26:25 +0300

    Created simple pygame screen

--- HololoGUI.py
+++ HololoGUI.py
@@ -1,1 +1,12 @@
+import pygame
+from pygame.locals import *
 
+width, height = 1280, 720
+
+pygame.init()
+pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
+
+
+while True:
+	pygame.display.flip()
+	pygame.time.wait(10)
