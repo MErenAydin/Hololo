7267a07f5f41f47bd4e5889592d270e55b246386
MErenAydin <merenep12@gmail.com>
2020-12-10 14:50:03 +0300

    Added mesh class

--- HololoGUI.py
+++ HololoGUI.py
@@ -2,6 +2,24 @@ import pygame
 from pygame.locals import *
 import pyassimp
 
+class Mesh:
+	def __init__(self, scene, x = 0, y = 0, z = 0, scale = 1, mesh_index = 0):
+		self.scene = scene
+		self.normals = scene.meshes[mesh_index].normals
+		self.vertices = scene.meshes[mesh_index].vertices
+		self.x = x
+		self.y = y
+		self.z = z
+		self.size = scale
+
+	def move(x, y, z):
+		self.x += x
+		self.y += y
+		self.z += z
+
+
+
+
 # Initialization of Window
 width, height = 1280, 720
 pygame.init()
@@ -10,8 +28,7 @@ pygame.display.set_caption("Hololo")
 
 # Testing and Importing Meshes With Assimp (https://www.assimp.org)
 scene = pyassimp.load("Template/cube.stl")
-model_normals = scene.meshes[0].normals
-model_vertices = scene.meshes[0].vertices
+test_mesh = Mesh(scene)
 
 
 # Main Loop
