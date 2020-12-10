52c1cce370443ef7e6ebc1087b099306a1f7360e
MErenAydin <merenep12@gmail.com>
2020-12-10 16:37:43 +0300

    Rendered Test Cube

--- HololoGUI.py
+++ HololoGUI.py
@@ -7,35 +7,19 @@ import struct
 from pyrr import Matrix44,Quaternion,Vector3
 
 class Mesh:
-	def __init__(self, scene, x = 0, y = 0, z = 0, scale = 1, mesh_index = 0, v_shader_path = "Shaders/model_v.shader" , f_shader_path = "Shaders/model_f.shader"):
+	def __init__(self, scene, x = 0, y = 0, z = 0, scale = 1, mesh_index = 0):
 		self.scene = scene
 		self.normals = scene.meshes[mesh_index].normals
 		self.vertices = scene.meshes[mesh_index].vertices
 		self.x = x
 		self.y = y
 		self.z = z
-		self.size = scale
-		self.context = moderngl.create_context()
-		self.program = self.context.program(
-			vertex_shader = open(v_shader_path).read(),
-			fragment_shader = open(f_shader_path).read(),
-			)
-		self.vbo = bytearray()
-		self.vao = bytearray()
+		self.scale = scale
 
-		# Uniform 4x4 Matrices variables
-		self._model = prog['model']
-		self._view = prog['view']
-		self._projection = prog['projection']
-		self._rotation = prog['rotation']
+		vertices = np.append(self.vertices, self.normals,1)
+		flatten = [j for i in vertices for j in i]
 
-		# Uniform Vector 3 variables
-		self._light_pos = prog['lightPos']
-		self._light_color = prog['lightColor']
-		self._color = prog['objectColor']
-
-		# Uniform Float Variable
-		self._scale = prog['scale']
+		self.context = Context(self.scale, flatten)
 
 
 	def move(self, x, y, z):
@@ -45,22 +29,52 @@ class Mesh:
 		model_mat = Matrix44.from_translation(np.array([self.y,self.x,self.z]))
 		self._model.write(model_mat.astype('float32').tobytes())
 
-	def init_mesh(self, light_pos = (0,0,0), light_color = (1,1,1), ):
-		self._light_pos.value = light_pos
-		self._light_color = light_color
+	def render(self):
+		pass
 
-		model_mat = Matrix44.from_translation(np.array([self.y,self.x,self.z]))
+class Context:
+	def __init__(self, scale, flatten, pos = (0,0,0), light_pos = (0,0,0), light_color = (1,1,1), v_shader_path = "Shaders/model_v.shader" , f_shader_path = "Shaders/model_f.shader"):
+		self.context = moderngl.create_context()
+		self.program = self.context.program(
+			vertex_shader = open(v_shader_path).read(),
+			fragment_shader = open(f_shader_path).read(),
+			)
+
+		# Uniform 4x4 Matrices variables
+		self._model = self.program['model']
+		self._view = self.program['view']
+		self._projection = self.program['projection']
+		self._rotation = self.program['rotation']
+
+		# Uniform Vector 3 variables
+		self._light_pos = self.program['lightPos']
+		self._light_color = self.program['lightColor']
+		self._color = self.program['objectColor']
+
+		# Uniform Float Variable
+		self._scale = self.program['scale']
+
+
+
+		self._light_pos.value = light_pos
+		self._light_color.value = light_color
+
+		model_mat = Matrix44.from_translation(np.array([pos[0],pos[1],pos[2]]))
 		self._model.write(model_mat.astype('float32').tobytes())
 
-		vertices = np.append(self.vertices, self.normals,1)
-		flatten = [j for i in vertices for j in i]
+		#vertices = np.append(self.vertices, self.normals,1)
+		#flatten = [j for i in vertices for j in i]
+
+		self._scale.write(struct.pack("f",scale))
 
 		self.vbo = self.context.buffer(struct.pack("{0:d}f".format(len(flatten)),*flatten))
 		self.vao = self.context.simple_vertex_array(self.program, self.vbo, 'aPos','aNormal')
 
-	def render(self):
-		pass
-
+		self.context.enable(moderngl.DEPTH_TEST)
+		self.context.enable(moderngl.BLEND)
+		self.context.enable(moderngl.CULL_FACE)
+		self.context.blend_func = (moderngl.SRC_ALPHA, moderngl.ONE_MINUS_SRC_ALPHA)
+		
 
 
 
@@ -77,9 +91,36 @@ test_mesh = Mesh(scene)
 
 
 # Main Loop
+
+init_look = Matrix44.look_at(
+        (10, 0.0, 2.5),
+        (0.0, 0.0, 0.0),
+        (0.0, 0.0, 1.0),
+)
+look_at = init_look.copy()
+rotate = Matrix44.identity()
+
 running = True
 while running:
+
+	proj = Matrix44.perspective_projection(45.0, width / height, 0.1, 1000.0)
+	test_mesh.context._projection.write(proj.astype('float32').tobytes())
+
+
+	test_mesh.context._view.write(look_at.astype('float32').tobytes())
+
+
+	test_mesh.context._rotation.write(rotate.astype('float32').tobytes())
+
+	test_mesh.context.context.viewport = (0, 0, width, height)
+	test_mesh.context.context.clear(0.7, 0.7, 0.9)
+
+	test_mesh.context._color.value = (0.67, 0.49, 0.29 , 1.0)
+	test_mesh.context.vao.render(moderngl.TRIANGLES)
+
 	pygame.display.flip()
 	pygame.time.wait(10)
+
+	
 
 pygame.quit()
