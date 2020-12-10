b8842d8ad5f3d1e4376de6142fa21e05637754ca
MErenAydin <merenep12@gmail.com>
2020-12-10 14:41:24 +0300

    Imported Assimp
    - Added some meshes for test

--- HololoGUI.py
+++ HololoGUI.py
@@ -1,12 +1,23 @@
 import pygame
 from pygame.locals import *
+import pyassimp
 
+# Initialization of Window
 width, height = 1280, 720
-
 pygame.init()
-pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
+window = pygame.display.set_mode((width, height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption("Hololo")
+
+# Testing and Importing Meshes With Assimp (https://www.assimp.org)
+scene = pyassimp.load("Template/cube.stl")
+model_normals = scene.meshes[0].normals
+model_vertices = scene.meshes[0].vertices
 
 
-while True:
+# Main Loop
+running = True
+while running:
 	pygame.display.flip()
 	pygame.time.wait(10)
+
+pygame.quit()
